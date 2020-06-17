4c13b500fa417ffcf30f14eba5fced6034865f08
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-17 16:27:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,7 +67,7 @@ class TicTacToe
   
   def won?
     WIN_COMBINATIONS.each do |winning_combos|
-      @board[winning_combos[0]] == @board[1] && @board[1] == @board[2]
+      @board[winning_combos[0]] == @board[winning_combos[1]] && @board[winning_combos[1]] == @board[winning_combos[2]]
       if winning_combos 
       binding.pry
     end 
